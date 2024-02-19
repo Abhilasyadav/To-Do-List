217c50216519db8e4b06ef7202226a6cd94f245e
Abhilasyadav <94737142+Abhilasyadav@users.noreply.github.com>
2024-02-20 00:59:54 +0530

    Add files via upload

---
+++ to-do-list.js
@@ -0,0 +1,37 @@
+const inputbox = document.getElementById("input-box");
+const list_container = document.getElementById("list-container");
+
+function addTask(){
+    if(inputbox.value === ''){
+        alert("You must write something!");
+    }
+    else{
+        let li = document.createElement("li");
+        li.innerHTML = inputbox.value;
+        list_container.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value = "";
+    saveData();
+}
+
+list_container.addEventListener("click", function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+    }
+    else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+    }
+},false); 
+
+function saveData(){
+    localStorage.setItem("data", list_container.innerHTML);
+}
+
+function showTask(){
+    list_container.innerHTML =localStorage.getItem("data");
+}
+
+showTask();
